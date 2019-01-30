79aa5de902583a11062c3d5499dc782fd4b9e962
gracieorozco <gracieorozco@csu.fullerton.edugit config --global user.name gracieorozcogit config --global user.email gracieorozco@csu.fullerton.edu>
2019-01-30 15:44:10 -0800

    Finished one function

--- review/review2.cpp
+++ review/review2.cpp
@@ -16,18 +16,32 @@ struct Student {
 // input into the student object passed in as an argument.
 Student* populate_fields(Student* x) {
     // TO BE COMPLETED
+    cout << "Input the CWID: ";
+    cin >> x->cwid;
+
     return x;
 }
 
 // Displays the student object passed in.
 void display_student(Student* x) {
     // TO BE COMPLETED
+    std::cout << "Int: " << x->cwid << std::endl;
+  /*  std::cout << "First Name: " << x->first << std::endl;
+    std::cout << "Last Name: " << x->last << std::endl;
+    std::cout << "Number of Courses: " << x->num_courses << std::endl;
+    for (int i = 0; i < x->num_courses; i++) {
+      cout << x->courses[i] << ", ";
+    }
+    std::cout << std::endl;
+    */
 }
 
 int main() {
     // Declare a Student object in the heap
-
+    Student *s1 = new Student;
+    popukate_fields
     // Call the functions that were completed
+    display_student(s1);
 
     return 0;
 }
